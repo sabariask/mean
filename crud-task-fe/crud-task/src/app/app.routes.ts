501d8fa9c9c83dd3958e16ddfdd32696e1658176
sabariask <sabarinathan.mani13@gmail.com>
2024-07-14 14:04:27 +0530

    Created new app as crud-task and added new component with bootstrap-scss

---
+++ crud-task-fe/crud-task/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { TaskScreenComponent } from './screens/task-screen/task-screen.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: TaskScreenComponent
+    }
+];
---
+++ crud-task-fe/crud-task/src/app/screens/task-screen/task-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-screen.component.html',
+  styleUrl: './task-screen.component.scss'
+})
+export class TaskScreenComponent {
+
+}
